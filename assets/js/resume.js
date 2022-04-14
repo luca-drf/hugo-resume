227eb833357fe92044220621a838c69aa58f95f5
Luca Da Rin Fioretto <radome@gmail.com>
2022-04-14 16:35:19 +0100

    Move css+js from static to assets & use hugo pipelines

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -15,6 +15,13 @@
     }
   });
 
+  // Tweaks img fitting based on layout
+  $('.resp-container').find('img').each(function(){
+    var imgClass = (this.width / this.height > 1) ? 'wide' : 'tall';
+    $(this).addClass(imgClass);
+   })
+
+
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
