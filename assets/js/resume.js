bde88372946a20d2eeff35644fa7e3bc040f0e71
Luca Da Rin Fioretto <ldarinfioretto@gmail.com>
2024-05-27 16:04:17 +0100

    Update jquery + use polyfill smoothscroll
    - Update jquery to 3.6.4
    - Replace jquery-easing custom script w/ smoothscroll-polyfill

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,26 +1,11 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
-
   // Tweaks img fitting based on layout
   $('.resp-container').find('img').each(function(){
     var imgClass = (this.width / this.height > 1) ? 'wide' : 'tall';
     $(this).addClass(imgClass);
    })
-
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
